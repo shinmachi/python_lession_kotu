452f11728f6c8ffd9bdc8c67f4e541810a5e6b11
daisuke_s88 <sinmachi@soka.ac.jp>
2017-09-05 10:05:09 +0900

    updte file

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,21 @@
 import random
 
+RANK, SUIT = 0, 1
+def get_point(hand):
+    result = 0
+    ace_flag = False
+    for card in hand:
+        if card[RANK] == 1:
+            ace_flag = True
+        if card[RANK] > 10:
+            num = 10
+        else:
+            num = card[RANK]
+        result = result + num
+    if ace_flag and result <= 11:
+        result += 10
+    return result
+
 def make_deck():
     suits = ['S','H','D','C']
     ranks = range(1,14)
@@ -18,13 +34,16 @@ def main():
         dealer_hand = [] # list of dealer hand
         deck = make_deck() # make dack
         # print(deck)
-        for i in range(2):
+        for i in range(2:
             player_hand.append(deck.pop())
             dealer_hand.append(deck.pop())
 
 
         print(player_hand)
+        print(get_point(player_hand))
+
         print(dealer_hand)
+        print(get_point(dealer_hand))
 
 
         turn += 1
