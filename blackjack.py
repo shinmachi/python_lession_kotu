5d67de2ca35e9ddc5ecc9ada3e4c198d1a00febf
daisuke shinmachi <sinmachi@soka.ac.jp>
2017-09-06 00:49:15 +0900

    update lesson code

--- blackjack.py
+++ blackjack.py
@@ -13,8 +13,31 @@ def get_point(hand):
             num = card[RANK]
         result = result + num
     if ace_flag and result <= 11:
-        result += 10
+        result += 10 # A = 11, add 10 to the result
     return result
+
+# print_player_hand function
+def print_player_hand(player_hand):
+    print('プレイヤー (', get_point(player_hand), '):   ')
+    for card in player_hand:
+        print('[', card[SUIT], card[RANK], ']')
+    print()
+
+# print_dealer_hand function
+def print_dealer_hand(dealer_hand, uncovered):
+    if uncovered:
+        print('ディーラー (', get_point(dealer_hand), '):   ')
+    else:
+        print('ディーラー ( ?? ):   ')
+    flag = True
+    for card in dealer_hand:
+        if  flag or uncovered:
+            print('[' , card[SUIT], card[RANK], ']')
+            flag = False
+        else:
+            print('[ * * ]')
+    print()
+
 
 def make_deck():
     suits = ['S','H','D','C']
@@ -34,16 +57,30 @@ def main():
         dealer_hand = [] # list of dealer hand
         deck = make_deck() # make dack
         # print(deck)
-        for i in range(2:
+        for i in range(2):
             player_hand.append(deck.pop())
             dealer_hand.append(deck.pop())
 
+        #print(player_hand)
+        print_player_hand(player_hand)
 
-        print(player_hand)
-        print(get_point(player_hand))
+        #print(dealer_hand)
+        print_dealer_hand(dealer_hand, False)
 
-        print(dealer_hand)
-        print(get_point(dealer_hand))
+        # プレイヤーターン
+        while True:
+            op = input('スタンド : 1,  ヒット : 2,  ダブル : 3  > ')
+            if op == '1':
+                print('[ プレイヤー : スタンド ]')
+                break
+            elif op == '2':
+                print('[ プレイヤー : ヒット ]')
+                player_hand.append(deck.pop())
+                print_player_hand(player_hand)
+            elif op == '3':
+                print('[ プレイヤー : ダブル ]')
+            else:
+                continue
 
 
         turn += 1
